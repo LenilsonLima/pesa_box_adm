21b12f26812280d73ec1220d3a374f1685fb4be8
LenilsonLima <lenilson.pantoja@estudante.ifms.edu.br>
2025-10-29 18:00:23 -0400

    29/10

--- src/pages/alterar/usuario/AlterarUsuario.js
+++ src/pages/alterar/usuario/AlterarUsuario.js
@@ -2,14 +2,15 @@ import { useNavigate, useOutletContext } from 'react-router-dom';
 import styles from './AlterarUsuario.module.scss';
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import axios from 'axios';
-import ApiUrl from '../../../apiUrl';
 import { RiUserLine } from 'react-icons/ri';
 import { useEffect, useState } from 'react';
+import ModalConfirmarCancelar from '../../../components/ModalConfirmarCancelar';
 
 const AlterarUsuario = () => {
     const navigation = useNavigate();
     const { setLoading } = useOutletContext();
     const [usuario, setUsuario] = useState({});
+    const [openCloseModalConfirmar, setOpenCloseModalConfirmar] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -67,10 +68,39 @@ const AlterarUsuario = () => {
         requestUsuario();
     }, []);
 
+    const desativarUsuario = async () => {
+        const token = localStorage.getItem('@pesabox_adm_token');
+        const id = localStorage.getItem('@pesabox_adm_id');
+        const requestOptions = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        };
 
-
+        try {
+            setLoading(true);
+            const body = { id: id, status: 0 };
+            const response = await axios.put('https://api-pesagem-chi.vercel.app/adm', body, requestOptions);
+            alert(response.data.retorno.mensagem);
+            localStorage.clear();
+            navigation('/login');
+        } catch (error) {
+            alert(error.response.data.retorno.mensagem);
+            console.log(error.response.data);
+        } finally {
+            setLoading(false);
+        }
+    }
     return (
         <form onSubmit={handleSubmit} className={styles.form}>
+            {openCloseModalConfirmar &&
+                <ModalConfirmarCancelar
+                    titulo={'Deseja desativar seu usuário? Você perderá todo o acesso ao sistema.'}
+                    funcConfirmar={() => desativarUsuario()}
+                    funcCancelar={() => setOpenCloseModalConfirmar(false)}
+                />
+            }
             <div className={styles.area_form}>
                 <div className={styles.input_form}>
                     <label>Nome</label>
@@ -97,7 +127,7 @@ const AlterarUsuario = () => {
                     </div>
                 </div>
                 <button className={styles.conectar}>Salvar Alteração</button>
-                <a onClick={() => navigation(-1)}>Desativar usuário</a>
+                <a onClick={() => setOpenCloseModalConfirmar(true)}>Desativar usuário</a>
             </div>
         </form>
     )
