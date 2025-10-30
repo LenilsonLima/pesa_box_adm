6fb8a00e5015fdf8ced6fe9ff63e170f7c54df60
LenilsonLima <lenilson.pantoja@estudante.ifms.edu.br>
2025-10-30 13:22:40 -0400

    30/10 3

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,9 +6,13 @@ import { useOutletContext } from 'react-router-dom';
 import ModalAcoes from '../../components/ModalAcoes';
 import ModalConfirmarCancelar from '../../components/ModalConfirmarCancelar';
 import { bloquearUsuario, buscarDados, removerDados } from '../../funcoes';
+import { IoIosSearch, IoMdArrowDropdown } from 'react-icons/io';
 
 const Home = () => {
+    const [pesquisar, setPesquisar] = useState('');
+    const [status, setStatus] = useState(3);
     const [usuarios, setUsuarios] = useState([]);
+    const [usuariosFiltrados, setUsuariosFiltrados] = useState([]);
     const [usuarioClicado, setUsuarioClicado] = useState({});
     const [openCloseModalAcoes, setOpenCloseModalAcoes] = useState(false);
     const [openCloseModalConfirmar, setOpenCloseModalConfirmar] = useState(false);
@@ -27,6 +31,22 @@ const Home = () => {
         requestUsuarios();
     }, []);
 
+    const handleFiltro = () => {
+        const termo = pesquisar?.toLowerCase() || "";
+        const usuariosFiltradostemp = usuarios?.filter((item) => {
+            const nomeMatch = item.nome?.toLowerCase().includes(termo);
+            const emailMatch = item.email?.toLowerCase().includes(termo);
+            const statusMatch = status == 3 ? true : String(item.status) === String(status);
+
+            return (nomeMatch || emailMatch) && statusMatch;
+        });
+
+        setUsuariosFiltrados(usuariosFiltradostemp);
+    };
+
+    useEffect(() => {
+        handleFiltro();
+    }, [pesquisar, status, usuarios]);
 
     const confirmarBloqueio = () => {
         setOpcaoSelecionadaModalConfirmar(1);
@@ -80,28 +100,42 @@ const Home = () => {
                     funcCancelar={() => setOpenCloseModalConfirmar(false)}
                 />
             }
-            {usuarios?.length > 0 &&
+            <div className={styles.area_filtro}>
+                <div className={styles.label_filtro}>
+                    <input type='text' placeholder='Nome ou e-mail' value={pesquisar} onChange={(e) => setPesquisar(e.target.value)} />
+                    <IoIosSearch />
+                </div>
+                <div className={styles.label_filtro}>
+                    <select value={status} onChange={(e) => setStatus(e.target.value)}>
+                        <option value="3">- mostrar todos -</option>
+                        <option value="1">Ativo</option>
+                        <option value="0">Inativo</option>
+                    </select>
+                    <IoMdArrowDropdown />
+                </div>
+            </div>
+            {usuariosFiltrados?.length > 0 &&
                 <table cellSpacing={0}>
                     <tbody>
-                        {usuarios?.map((usuario, index) => (
+                        {usuariosFiltrados?.map((usuario, index) => (
                             <tr onClick={() => handleUsuarioClicado(usuario)} key={usuario?.id}>
-                                <td style={{ borderBottomWidth: index === (usuarios?.length - 1) ? 0 : 1 }}>
+                                <td style={{ borderBottomWidth: index === (usuariosFiltrados?.length - 1) ? 0 : 1 }}>
                                     <div>
                                         <MdVerifiedUser style={{ backgroundColor: usuario?.status == 1 ? '#56a368ff' : '#cb2027' }} />
                                     </div>
                                 </td>
-                                <td style={{ borderBottomWidth: index === (usuarios?.length - 1) ? 0 : 1 }}>
+                                <td style={{ borderBottomWidth: index === (usuariosFiltrados?.length - 1) ? 0 : 1 }}>
                                     <div>
                                         <span style={{ color: usuario?.status == 1 ? '#000' : '#cb2027' }}>{usuario?.nome}</span>
                                         <span style={{ color: usuario?.status == 1 ? 'gray' : '#cb2027' }}>{usuario?.email}</span>
                                     </div>
                                 </td>
-                                <td style={{ borderBottomWidth: index === (usuarios?.length - 1) ? 0 : 1 }}>
+                                <td style={{ borderBottomWidth: index === (usuariosFiltrados?.length - 1) ? 0 : 1 }}>
                                     <div>
                                         <span style={{ color: usuario?.status == 1 ? '#000' : '#cb2027' }}>{usuario?.status == 1 ? 'Ativo' : 'Inativo'}</span>
                                     </div>
                                 </td>
-                                <td style={{ borderBottomWidth: index === (usuarios?.length - 1) ? 0 : 1 }}>
+                                <td style={{ borderBottomWidth: index === (usuariosFiltrados?.length - 1) ? 0 : 1 }}>
                                     <div>
                                         <GoKebabHorizontal onClick={() => handleUsuarioClicado(usuario)} style={{ color: usuario?.status == 1 ? '#000' : '#cb2027' }} />
                                     </div>
